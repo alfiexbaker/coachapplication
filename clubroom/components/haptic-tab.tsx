9b15374f90fc1324a5ca4d63452a4559a259afa2
alfiexbaker <88007808+alfiexbaker@users.noreply.github.com>
2025-11-20 11:13:37 +0000

    Improve tab navigation and message bubble contrast

--- clubroom/components/haptic-tab.tsx
+++ clubroom/components/haptic-tab.tsx
@@ -1,10 +1,19 @@
 import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
 import { PlatformPressable } from '@react-navigation/elements';
 import * as Haptics from 'expo-haptics';
+import { Link } from 'expo-router';
+import { forwardRef } from 'react';
+import { Platform, type View } from 'react-native';
 
-export function HapticTab(props: BottomTabBarButtonProps) {
-  return (
+export const HapticTab = forwardRef<View, BottomTabBarButtonProps>(function HapticTab(
+  { href, ...props },
+  ref
+) {
+  const content = (
     <PlatformPressable
+      ref={ref}
+      accessibilityRole="tab"
+      hitSlop={8}
       {...props}
       onPressIn={(ev) => {
         if (process.env.EXPO_OS === 'ios') {
@@ -15,4 +24,14 @@ export function HapticTab(props: BottomTabBarButtonProps) {
       }}
     />
   );
-}
+
+  if (Platform.OS === 'web' && href) {
+    return (
+      <Link href={href} asChild>
+        {content}
+      </Link>
+    );
+  }
+
+  return content;
+});
