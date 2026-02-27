6457cc41c358866ee8cb2d76ff60380947b84c7d
alfiexbaker <williamalfredbaker@gmail.com>
2026-02-27 20:37:07 +0000

    chore(cleanup): remove dead messaging composer and attachment cluster

--- clubroom/components/messaging/index.ts
+++ clubroom/components/messaging/index.ts
@@ -1,6 +1,3 @@
-export * from './attachment-picker';
 export * from './chat-input';
 export * from './message-bubble';
-export * from './message-composer';
-export * from './thread-summary';
 export * from './typing-indicator';
