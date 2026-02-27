a03b9cb84da988c7ce494bf41d2cdf1c8c433633
alfiexbaker <williamalfredbaker@gmail.com>
2026-02-27 20:53:59 +0000

    chore(cleanup): remove dead safety modal/banner/report cluster

--- clubroom/components/safety/index.ts
+++ clubroom/components/safety/index.ts
@@ -2,7 +2,3 @@ export * from './EmergencyContactCard';
 export * from './EmergencyQuickCard';
 export * from './MedicalAlertBadge';
 export * from './SafetyChecklist';
-export * from './emergency-banner';
-export * from './medical-card';
-export * from './report-flow';
-export * from './block-user';
