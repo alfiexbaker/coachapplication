a4645b795417eae7dc70a4836148650559313636
alfiexbaker <williamalfredbaker@gmail.com>
2026-05-06 11:19:38 +0100

    refactor(club): drop barrel-only dead dashboard panels

--- clubroom/components/club/index.ts
+++ clubroom/components/club/index.ts
@@ -4,14 +4,8 @@ export type { ClubHeaderProps, ClubStatsRowProps } from './ClubHeader';
 export { MemberRow, MembersPanel } from './MembersPanel';
 export type { MemberRowProps, MembersPanelProps } from './MembersPanel';
 
-export { SessionsPanel } from './SessionsPanel';
-export type { SessionsPanelProps } from './SessionsPanel';
-
 export { ClubActivitiesPanel } from './ClubActivitiesPanel';
 export type { ClubActivitiesPanelProps } from './ClubActivitiesPanel';
-
-export { MatchesPanel } from './MatchesPanel';
-export type { MatchesPanelProps } from './MatchesPanel';
 
 export { FeedPost } from './FeedPost';
 export type { FeedPostProps } from './FeedPost';
